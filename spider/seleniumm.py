e1fbef37f80e95b6f1f135548fe183b2940fa51b
daigouwei <daigouwei@gmail.com>
2017-05-31 21:10:56 +0800

    继续增加seleniumm.py中相关内容

--- spider/seleniumm.py
+++ spider/seleniumm.py
@@ -8,7 +8,7 @@ browser = webdriver.Chrome()
 browser.get('http://www.baidu.com')
 '''
 
-# '''2
+'''2
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -19,3 +19,19 @@ elem = driver.find_element_by_name('q')
 elem.send_keys('pycon')
 elem.send_keys(Keys.ENTER)
 print(driver.page_source)
+'''
+
+# '''3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get('http://somedomain/url_that_delays_loading')
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'myDynamicElement'))
+    )
+finally:
+    driver.quit()
