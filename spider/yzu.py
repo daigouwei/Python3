38d549e7dfac8b24ac83fe2e357d3aa05371096d
daigouwei <daigouwei@gmail.com>
2017-06-02 01:15:31 +0800

    完善了yzu.py实现了获取信息和照片功能，但是还有信息处理部分需要用beautifulsoup进一步完善处理。

--- spider/yzu.py
+++ spider/yzu.py
@@ -4,6 +4,8 @@
 # import time
 import requests
 from selenium import webdriver
+import re
+from bs4 import BeautifulSoup
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
@@ -12,27 +14,27 @@ from selenium import webdriver
 username = input('请输入用户名：')
 password = input('请输入密码：')
 url = 'http://yjsglxt.yzu.edu.cn/pyxx/login.aspx'
-broswer = webdriver.PhantomJS()
-broswer.set_window_size(900, 900)#不能跟Chrome一样全屏，原因未知。
+browser = webdriver.PhantomJS()
+# browser.set_window_size(900, 900)#不能跟Chrome一样全屏，原因未知。
 
-# 使用Chrome截图会是黑色图，可以全屏，原因未知。
-# broswer = webdriver.Chrome()
-# broswer.maximize_window()
+# 使用Chrome截图会是黑色图，可以全屏，原因未知。在mac上没有问题。
+# browser = webdriver.Chrome()
+browser.maximize_window()
 
-broswer.get(url)
+browser.get(url)
 
 # time.sleep(10)
-# broswer.implicitly_wait(30)
+# browser.implicitly_wait(30)
 
-broswer.save_screenshot('yzm.png')
-elemUsername = broswer.find_element_by_name('ctl00$txtusername')
+browser.save_screenshot('yzm.png')
+elemUsername = browser.find_element_by_name('ctl00$txtusername')
 elemUsername.send_keys(username)
-elemPassword = broswer.find_element_by_name('ctl00$txtpassword')
+elemPassword = browser.find_element_by_name('ctl00$txtpassword')
 elemPassword.send_keys(password)
-elemYzm = broswer.find_element_by_name('ctl00$txtyzm')
+elemYzm = browser.find_element_by_name('ctl00$txtyzm')
 
 # 由于yzu的验证码是纯动态的，就算拿到具体时间的验证码网址，会发现两次访问同一个网址验证码也是不一样的，所以注释方法不行。
-# yzmLink = broswer.find_element_by_id('myCode').get_attribute('src')
+# yzmLink = browser.find_element_by_id('myCode').get_attribute('src')
 # yzmRe = requests.get(yzmLink)
 # gifData = yzmRe.content.split(b'\r\n', 1)
 # with open('yzm.gif', 'wb') as f:
@@ -40,17 +42,20 @@ elemYzm = broswer.find_element_by_name('ctl00$txtyzm')
 
 yzm = input('请打开验证码图yzm.png，输入验证码：')
 elemYzm.send_keys(yzm)
-elemAccount = broswer.find_element_by_name('ctl00$ImageButton1')
+elemAccount = browser.find_element_by_name('ctl00$ImageButton1')
 elemAccount.click()
-# print(broswer.page_source)
-broswer.switch_to_frame('PageFrame')#有深层frame需要切换frame抓取
-# print(broswer.page_source)
-photoUrl = broswer.find_element_by_id('imgPhoto').get_attribute('src')
-photoRe = requests.get(photoUrl)
-photoData = photoRe.content
-with open('photo.jpg', 'wb') as f:
-    f.write(photoData)
-
-broswer.quit()
+# print(browser.page_source)
+browser.switch_to_frame('PageFrame')#有深层frame需要切换frame抓取
+# print(browser.page_source)
+soup = BeautifulSoup(browser.page_source, 'lxml')
+soupStr = soup.find_all(style='font-weight:bold;')
+print(soupStr)
+# print(soupStr[0])
+# soup2 = BeautifulSoup(str( soup1Str[0] ), 'lxml')
+# print(soup2.span.string)
+#下面两行用Chrome是可以直接下载图片的，在PhantomJS下没任何反应
+photoUrl = browser.find_element_by_id('imgPhoto').get_attribute('src')
+browser.get(photoUrl)
+browser.quit()
 
 
