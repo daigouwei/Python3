a655c168ade13a76a244ad6a7cb3083c71a3f217
guowei2 <guowei2@pset.suntec.net>
2017-06-19 17:07:50 +0800

    tickets.py中添加了命令行解析工具以及@log相关使用

--- app/tickets.py
+++ app/tickets.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env
 #-*- coding:utf-8 -*-
 
+"""命令行火车票查看器
+Usage:
+    tickets.py [-gdtkz] <from> <to> <date>
 
+Options:
+    -h,--help   显示帮助菜单
+    -g          高铁
+    -d          动车
+    -t          特快
+    -k          快速
+    -z          直达
+
+Example:
+    tickets.py 北京 上海 2016-10-10
+    tickets.py -dg 成都 南京 2016-10-10
+"""
+
+import logging
+def log(func):
+    def wrapper(*argv, **kw):
+        print('###################################################')
+        logging.warn(func.__name__)
+        print('###################################################')
+        return func(*argv, **kw)
+    return wrapper
+
+from docopt import docopt
+@log
+def commandLineInterface():
+    arguments = docopt(__doc__)
+    # print(arguments)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+    # commandLineInterface()
