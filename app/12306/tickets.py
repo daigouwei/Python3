3c0c17ed393116c765446f1ebf303772b671239a
guowei2 <guowei2@pset.suntec.net>
2017-06-28 21:51:47 +0800

    增加了显示打框功能，还需要增加颜色，起始站并排，打包工作

--- app/12306/tickets.py
+++ app/12306/tickets.py
@@ -19,18 +19,53 @@ Example:
 """
 
 import logging
-def log(func):
-    def wrapper(*argv, **kw):
-        print('###################################################')
-        logging.warn(func.__name__)
-        print('###################################################')
-        return func(*argv, **kw)
-    return wrapper
-
 from docopt import docopt
 from stations import stations
 import requests
 import re
+from prettytable import PrettyTable
+
+def log(func):
+    def wrapper(*argv, **kw):
+        logging.warn(func.__name__)
+        return func(*argv, **kw)
+    return wrapper
+
+@log
+def getKey(stations, value):
+    return [k for k,v in stations.items() if value == v]
+
+@log
+def handleData(data):
+    newData = []
+    for sf in data:
+        newData.append(list(sf))
+    for sf in newData:
+        sf[1] = getKey(stations, sf[1])[0]
+        sf[2] = getKey(stations, sf[2])[0]
+        sf[5] = sf[5].replace(':','h')+'min'
+    return newData
+
+@log
+def prettyPrint(data):
+    headline = '1 2 3 4 5 6 7 8 9 10 11 12'.split()
+    pt = PrettyTable()
+    pt._set_field_names(headline)
+    for sf in data:
+        pt.add_row(sf)
+    print(pt)
+
+@log
+def getWebData(date, fromStation, toStation):
+    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date, fromStation, toStation)
+    requests.packages.urllib3.disable_warnings()
+    response = requests.get(url, verify=False)
+    # print(response.text)
+    pattern = re.compile(r'\|预订\|.*?\|([A-Z][0-9]+)\|.*?\|.*?\|([A-Z]+)\|([A-Z]+)\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|.*?\|\d{8}\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|.*?\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|', re.S)
+    data = pattern.findall(response.text)
+    # print(data)
+    return data
+
 @log
 def commandLineInterface():
     arguments = docopt(__doc__)
@@ -38,20 +73,9 @@ def commandLineInterface():
     fromStation = stations.get(arguments['<from>'])
     toStation = stations[arguments['<to>']]
     date = arguments['<date>']
-    print(fromStation, toStation, date)
-    url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT'.format(date, fromStation, toStation)
-    requests.packages.urllib3.disable_warnings()
-    response = requests.get(url, verify=False)
-    # print(response.text)
-    pattern = re.compile(r'\|预订\|.*?\|([A-Z][0-9]+)\|.*?\|.*?\|([A-Z]+)\|([A-Z]+)\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|(\d{2}:\d{2})\|.*?\|\d{8}\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|.*?\|.*?\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|([\u4e00-\u9fa5]|[0-9]+|)\|', re.S)
-    data = pattern.findall(response.text)
-    print(data)
-
-
-
-
-
-
+    # print(fromStation, toStation, date)
+    newData = handleData(getWebData(date, fromStation, toStation))
+    prettyPrint(newData)
 
 
 
