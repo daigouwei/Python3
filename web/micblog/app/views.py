5e4d84a78723a04257cd8ed6bd9afdb31dc32685
guowei2 <guowei2@pset.suntec.net>
2017-07-25 20:55:31 +0800

    complete form function

--- web/micblog/app/views.py
+++ web/micblog/app/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env
 #-*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 from app.init_app import app
 
@@ -27,6 +27,14 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    print('###[flag1]###')
+    if form.validate_on_submit():
+        print('###[flag2]###')
+        flash('Login requested for Name: %s' % form.name.data)
+        flash('password: %s' % str(form.password.data))
+        flash('remember_me: %s' % str(form.remember_me.data))
+        return redirect('/index')
+    print('###[flag3]###')
     return render_template('login.html',
                             title='Sign In',
                             form = form)
