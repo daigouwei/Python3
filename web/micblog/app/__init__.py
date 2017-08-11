3b7fc0e765ec411bbe6ee08df77426639e7dbb21
guowei2 <guowei2@pset.suntec.net>
2017-08-11 21:04:27 +0800

    [micblog] reconfigure the user model

--- web/micblog/app/__init__.py
+++ web/micblog/app/__init__.py
@@ -3,9 +3,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')   #读取配置文件config
+
 db = SQLAlchemy(app)      #实例化数据库ORM
+
+lm = LoginManager()    #初始化flask-login
+lm.setup_app(app)
 
 from app import views, models
--- web/micblog/app/views.py
+++ web/micblog/app/views.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env
 #-*- coding:utf-8 -*-
 
-from flask import render_template, flash, redirect
+import datetime
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
-from app import app
+from app.models import User, Post, ROLE_USER, ROLE_ADMIN
+from app import app, db, lm
+
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 @app.route('/index')
@@ -26,12 +33,34 @@ def index():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect('/index')
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for Name: ' + form.name.data)
-        flash('passwd: ' + str(form.password.data))
-        flash('remember_me: ' + str(form.remember_me.data))
-        return redirect('/index')           #告诉网页浏览器引导到一个不同的页面而不是请求的页面。
+        user = User.login_check(request.form.get('user_name'))
+        if user:
+            login_user(user)
+            user.last_seen = datetime.datetime.now()
+
+            try:
+                db.session.add(user)
+                db.session.commit()
+            except:
+                flash("The Database error!")
+                return redirect('/login')
+       
+            flash('Your name: ' + request.form.get('user_name'))
+            flash('remember me? ' + str(request.form.get('remember_me')))
+            return redirect('/login')
+
+    #form = LoginForm()
+    #if form.validate_on_submit():
+    #    flash('Login requested for Name: ' + form.name.data)
+    #    flash('passwd: ' + str(form.password.data))
+    #    flash('remember_me: ' + str(form.remember_me.data))
+    #    return redirect('/index')           #告诉网页浏览器引导到一个不同的页面而不是请求的页面。
     return render_template('login.html',
                            title = 'Sign In',
                            form = form)
+
+
